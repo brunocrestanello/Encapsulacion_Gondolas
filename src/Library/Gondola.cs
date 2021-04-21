001e93296e765568ce0e7f3fd54cacf8f15b708e
Rafael Durán <duran.r.m.g@gmail.com>
2021-04-20 23:08:54 -0300

    Dejé más prolijo lo que se imprime por consola.

--- src/Library/Gondola.cs
+++ src/Library/Gondola.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel.ReadOnlyCollection;|
+using System.Collections.ObjectModel;
 
 namespace Library
 {
     public class Gondola
     {
+        private string nombreGondola;
+
+        public string NombreGondola
+        {
+            get
+            {
+                return nombreGondola;
+            }
+        }
 
         private List<Producto> estanteria;
 
@@ -20,22 +29,22 @@ namespace Library
             }
         }
 
-
-        public Gondola()
+        public Gondola(string nombreGondola)
         {
+            this.nombreGondola = nombreGondola;
             this.estanteria = new List<Producto>();
         }
 
-        
-
         public void AgregarProducto(Producto producto)
         {
-            Estanteria.Add(producto);
+            estanteria.Add(producto);
+            Console.WriteLine("Se ha AGREGADO el producto: " + producto.Nombre +" a la estantería de la "+ this.nombreGondola);
         }
 
         public void QuitarProducto(Producto producto)
         {
-            Estanteria.Remove(producto);
+            estanteria.Remove(producto);
+            Console.WriteLine("Se ha REMOVIDO el producto: " + producto.Nombre +" de la estantería de la " + this.nombreGondola);
         }
     }
 }
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Library;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace Program
 {
@@ -8,14 +9,23 @@ namespace Program
     {
         static void Main(string[] args)
         {   
-            List<Producto> Estanteria = new List<Producto>();
-            Producto arroz = new Producto(5514468, "Saman", 90);
-            Producto jabon = new Producto(4496858, "Dove", 100);
-            Producto escoba = new Producto(852015, "Brujita", 250);
+            Gondola gondola1 = new Gondola("Góndola de alimentos");
+            Gondola gondola2 = new Gondola("Góndola de limpieza");
+            Gondola gondola3 = new Gondola("Góndola de higiene personal");
+            
+            Producto arroz = new Producto(5514468, "Arroz Samán", 90);
+            Producto jabon = new Producto(4496858, "Jabón Dove", 100);
+            Producto escoba = new Producto(852015, "Escoba Alonso & Mariños", 250);
+            Producto pastaDental = new Producto(452037, "Pasta de dientes Colgate", 150);
 
-            Estanteria.Add(arroz);
-        
+            gondola1.AgregarProducto(arroz);        
+            gondola1.AgregarProducto(escoba);
+            gondola1.QuitarProducto(escoba);
 
+            gondola2.AgregarProducto(escoba);
+            gondola2.AgregarProducto(jabon);
+
+            gondola3.AgregarProducto(pastaDental);
         }
     }
 }
